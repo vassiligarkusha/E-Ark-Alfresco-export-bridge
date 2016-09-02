94fd7544cfaf92c02f00e8a6a39c6d28e0f0d311
Andreas Kring <andreas@magenta.dk>
2016-09-02 07:59:44 +0200

    Begun work on EadBuilder used to build the final EAD

--- src/main/java/dk/magenta/eark/erms/ead/EadBuilder.java
+++ src/main/java/dk/magenta/eark/erms/ead/EadBuilder.java
@@ -1,5 +1,25 @@
 package dk.magenta.eark.erms.ead;
 
+import java.io.InputStream;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+
 public class EadBuilder {
+	
+	private Document ead;
+	private XmlHandler xmlHandler;
+	
+	/**
+	 * 
+	 * @param in
+	 * @throws JDOMException Throws exception if the provided ead.xml is not valid
+	 */
+	public EadBuilder(InputStream in) throws JDOMException {
+		xmlHandler = new XmlHandlerImpl();
+		ead = xmlHandler.readAndValidateXml(in, "ead3.xsd");
+	}
+	
+	
 	
 }
